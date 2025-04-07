88694eef181bd74c5bbdd3ced38a7d96663239c1
Bernardo Chamilet <bernardoboggionechamilet@gmail.com>
2025-04-06 23:28:29 -0300

    Implementação rota de deletar relação receiver item

--- src/routes/receivers_routes.mjs
+++ src/routes/receivers_routes.mjs
@@ -1,7 +1,9 @@
 import express from 'express';
 import * as controllers from '../controllers/receivers_controllers.mjs';
-import { verificarToken, verificarTokenEID } from '../middlewares/auth.mjs';
+import { verificarTokenEID } from '../middlewares/auth.mjs';
 
 export const receiver_routes = express.Router();
 
 receiver_routes.post('/receiver/:id/:item_id', verificarTokenEID, controllers.receiveItem);
+
+receiver_routes.delete('/receiver/:id/:item_id', verificarTokenEID, controllers.deleteReceiverRelation)
